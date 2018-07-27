70796b8b67b77fe78e02fe5ed70792e887234d91
namrutha <namruthabv12@gmail.com>
2018-07-27 00:34:48 -0700

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   get 'posts/edit'
   get 'posts/destroy'
   get 'posts/update'
+  get 'posts/create'
+
+  root 'posts#index'
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
